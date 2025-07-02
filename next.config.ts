a80bb2de63175c24c819c9cc9dd1d323d35693c4
Ahamed Shahmy <maa.shahmy@gmail.com>
2025-07-02 15:31:10 +0000

    can you publish this in firebase hosting

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,6 @@
 import type {NextConfig} from 'next';
 
-// IMPORTANT: Update this to your repository name
-const repositoryName = 'nephrosim';
-
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  // Required for static export
-  images: {
-    unoptimized: true,
-  },
-  // Add assetPrefix and basePath for GitHub Pages
-  basePath: isGithubActions ? `/${repositoryName}` : '',
-  assetPrefix: isGithubActions ? `/${repositoryName}/` : '',
-
   typescript: {
     ignoreBuildErrors: true,
   },
