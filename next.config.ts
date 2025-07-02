6d453b22778e0cbbfe46e02655f4bceaafc76338
Ahamed Shahmy <maa.shahmy@gmail.com>
2025-07-02 15:36:49 +0000

    please change it to be able to be uploaded in github pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type {NextConfig} from 'next';
 
+const repositoryName = 'nephrosim';
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: isProd && repositoryName ? `/${repositoryName}` : '',
+  assetPrefix: isProd && repositoryName ? `/${repositoryName}/` : '',
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
